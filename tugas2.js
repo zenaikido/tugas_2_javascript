8f5aa9ad9da95fc8f3f277fd08a86784cc617ee7
Rauf <zen.star12@yahoo.com>
2021-01-24 15:57:38 +0700

    Tugas 2 Javascript
    Tugas 2 Javascript Niomic

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+var angka = 1;
+var setring = "Nama muin";
+var undif ;
+var arai = [1,2,3,4];
+var lean = true;
+
+if(angka>2){
+    lean=false;
+}else{
+    lean=true;
+}
+
+console.log(angka);
+
+console.log(setring);
+
+console.log(undif);
+
+console.log(arai[1]);
+
+console.log(lean);
